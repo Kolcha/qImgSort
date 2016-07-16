f88410072d8e2d0b3f0684d8e615cd65c854b664
Nick Korotysh <nick.korotysh@gmail.com>
2016-07-16 20:39:05 +0300

    added tooltips

--- qimgsort_ru.ts
+++ qimgsort_ru.ts
@@ -15,19 +15,50 @@
     </message>
     <message>
         <location filename="main_window.ui" line="61"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Delete processed files from source directory (files will be &lt;span style=&quot; font-weight:600;&quot;&gt;moved&lt;/span&gt; to target directory).&lt;/p&gt;&lt;p&gt;Source directory will be removed if there is no more files in it.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Удалять обработанные файлы с исходной папки (файлы будут &lt;span style=&quot; font-weight:600;&quot;&gt;перемещены&lt;/span&gt; в целевую папку).&lt;/p&gt;&lt;p&gt;Исходная папка будет удалена если в ней неосталось файлов.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="main_window.ui" line="64"/>
         <source>Delete processed</source>
         <translation>Удалять обработанные</translation>
     </message>
     <message>
-        <location filename="main_window.ui" line="71"/>
+        <location filename="main_window.ui" line="74"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Change file extension to correct if incorrect extension will detected.&lt;/p&gt;&lt;p&gt;Unsupported files will not changed.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Изменить расширение файла если будет замечено некорректное.&lt;/p&gt;&lt;p&gt;Неподдерживаемы файлы переименованы не будут.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="main_window.ui" line="77"/>
         <source>Fix extensions</source>
         <translation>Исправлять расширения</translation>
     </message>
     <message>
-        <location filename="main_window.ui" line="81"/>
+        <location filename="main_window.ui" line="87"/>
         <location filename="main_window.cpp" line="86"/>
         <source>GO!</source>
         <translation>Старт!</translation>
+    </message>
+    <message>
+        <location filename="main_window.ui" line="99"/>
+        <source>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;.Lucida Grande UI&apos;; font-size:13pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p align=&quot;center&quot; style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-weight:600; color:#ff0000;&quot;&gt;IMPORTANT&lt;/span&gt;&lt;/p&gt;
+&lt;p style=&quot; margin-top:12px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; color:#0000ff;&quot;&gt;1.&lt;/span&gt; This program does the sorting images by &lt;span style=&quot; font-weight:600;&quot;&gt;resolution&lt;/span&gt;, nothing more.&lt;/p&gt;
+&lt;p style=&quot; margin-top:12px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; color:#0000ff;&quot;&gt;2.&lt;/span&gt; This program supports about 15 images formats, any unsupported files (even not images) will placed to &amp;quot;&lt;span style=&quot; font-style:italic;&quot;&gt;unsupported&lt;/span&gt;&amp;quot; folder.&lt;/p&gt;
+&lt;p style=&quot; margin-top:12px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; color:#0000ff;&quot;&gt;3.&lt;/span&gt; This program relies on file content, not extension, and even can fix incorrect file extension when it detected.&lt;/p&gt;
+&lt;p style=&quot; margin-top:12px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; color:#0000ff;&quot;&gt;4.&lt;/span&gt; If file with same name already exists in target directory, new file will be renamed (number will appended to its name), no overwrites is guaranted.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;.Lucida Grande UI&apos;; font-size:13pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p align=&quot;center&quot; style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-weight:600; color:#ff0000;&quot;&gt;ВАЖНАЯ ИНФОРМАЦИЯ&lt;/span&gt;&lt;/p&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; color:#0000ff;&quot;&gt;1.&lt;/span&gt; Программа предназначена для сортировки картинок по &lt;span style=&quot; font-weight:600;&quot;&gt;разрешению&lt;/span&gt;, ничего более.&lt;/p&gt;
+&lt;p style=&quot; margin-top:12px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; color:#0000ff;&quot;&gt;2.&lt;/span&gt; Программа &amp;quot;понимает&amp;quot; около 15 форматов изображений, любые неподдерживаемые файлы (даже вовсе не картинки) будум помещены в папку &amp;quot;&lt;span style=&quot; font-style:italic;&quot;&gt;неподдерживаемые&lt;/span&gt;&amp;quot;.&lt;/p&gt;
+&lt;p style=&quot; margin-top:12px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; color:#0000ff;&quot;&gt;3.&lt;/span&gt; Программа НЕ &amp;quot;смотрит&amp;quot; на расширение файла, она анализирует содержимое, и даже может исправить некорректное расширение если такое будет обнаружено.&lt;/p&gt;
+&lt;p style=&quot; margin-top:12px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; color:#0000ff;&quot;&gt;4.&lt;/span&gt; Если в конечной папке существует файл с таким же именем, новый файл будет переименован (к его имени будет добавлена цифра), перезапись файлов невозможна.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
         <location filename="main_window.cpp" line="70"/>
